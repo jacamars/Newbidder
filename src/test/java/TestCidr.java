b4514b40365f3b98326f566bdebfcc1b7b60bd03
Ben Faul <ben.faul@gmail.com>
2022-04-20 10:35:46 -0700

    Add  new tests to ensure negative numbers are not returned on the cidr list

--- src/test/java/TestCidr.java
+++ src/test/java/TestCidr.java
@@ -2,13 +2,18 @@ package test.java;
 
 import static org.junit.Assert.*;
 
-
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.jacamars.dsp.rtb.bidder.Controller;
+import com.jacamars.dsp.rtb.blocks.CIDRUtils;
 import com.jacamars.dsp.rtb.blocks.LookingGlass;
 import com.jacamars.dsp.rtb.blocks.NavMap;
 
@@ -58,5 +63,33 @@ public class TestCidr  {
 		    p = sr.contains("166.55.255.255");
 		    assertFalse(p);
 
+	  }
+	  
+	
+	  @Test
+	  public void testForNegative() throws Exception {
+		  var cidr = new CIDRUtils("191.255.255.254/32");
+		  
+		  long a = cidr.getStartAddress() ;
+		  long b = cidr.getEndAddress();
+		  
+		  assertTrue(a>0);
+		  assertTrue(b>0);
+		  assertTrue(a==b);
+	  }
+	  
+	  @Test
+	  public void testInRange() throws Exception {
+		  List<String> addrs = new ArrayList<>();
+		  
+		  addrs.add("45.33.224.0/20");
+		  
+		  var cidr = new CIDRUtils("45.33.224.0/20");
+		  boolean p = cidr.isInRange("45.33.224.0");
+		  
+		  
+		  cidr = new CIDRUtils(addrs);
+		  p = cidr.isInRange("45.33.224.0");
+		  assertTrue(p);
 	  }
 }
